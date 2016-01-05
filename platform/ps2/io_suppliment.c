664cd9dccecb909a11f2db95f3570b029a7451a3
fjtrujy <fjtrujy@gmail.com>
2016-01-05 19:05:37 +0100

    commit adding changes for ps2 done by sp193

---
+++ platform/ps2/io_suppliment.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <fileXio.h>
+#include <unistd.h>
+
+#include "ps2.h"
+
+int ps2_remove(const char *file){
+	char cwd[FILENAME_MAX], path[FILENAME_MAX];
+	int PathLength;
+
+	getcwd(cwd, sizeof(cwd));
+	PathLength=strlen(cwd);
+	if(PathLength>=1 && (cwd[PathLength-1]=='/' || cwd[PathLength-1]=='\\')){
+		cwd[--PathLength]='\0';
+	}
+
+	if(strchr(cwd, ':')!=NULL){
+		sprintf(path, "%s/%s", cwd, file);
+	}
+	else{
+		sprintf(path, "host:%s/%s", cwd, file);
+	}
+	return remove(path);
+}
+
+int ps2_mkdir(const char *path, int mode){
+	char cwd[FILENAME_MAX], FullPath[FILENAME_MAX];
+	int PathLength;
+
+	getcwd(cwd, sizeof(cwd));
+	PathLength=strlen(cwd);
+	if(PathLength>=1 && (cwd[PathLength-1]=='/' || cwd[PathLength-1]=='\\')){
+		cwd[--PathLength]='\0';
+	}
+
+	if(strchr(cwd, ':')!=NULL){
+		sprintf(FullPath, "%s/%s", cwd, path);
+	}
+	else{
+		sprintf(FullPath, "host:%s/%s", cwd, path);
+	}
+	return fileXioMkdir(FullPath, mode);
+}
---
+++ platform/ps2/version.h
@@ -0,0 +1,2 @@
+#define VERSION "1.51b"
+#define PORT_VERSION "1.13"
