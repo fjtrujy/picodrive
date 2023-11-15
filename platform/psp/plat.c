4ba7e0a22f53b2530720a1fc8516784dae65d19a
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-11-15 21:27:23 +0100

    Clean-up
    More cleans

--- platform/psp/plat.c
+++ platform/psp/plat.c
@@ -3,6 +3,7 @@
  *
  * (C) 2020 kub
  */
+#include <stdint.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,18 +11,19 @@
 #include <malloc.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 #include "../common/emu.h"
+#include "../common/version.h"
 #include "../libpicofe/menu.h"
 #include "../libpicofe/plat.h"
 
-#include <pspiofilemgr.h>
+#include <pspkernel.h>
 #include <pspthreadman.h>
 #include <pspdisplay.h>
 #include <psputils.h>
 #include <psppower.h>
 #include <pspgu.h>
-#include <pspaudio.h>
 
 #include "psp.h"
 #include "emu.h"
@@ -42,6 +44,132 @@
  * Menu background uses native resolution and is copied and shaded from a frame
  * buffer, or read in from a file if no emulator screen output is present.
  */
+
+PSP_MODULE_INFO("PicoDrive", 0, 1, 97);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER|THREAD_ATTR_VFPU);
+
+unsigned int __attribute__((aligned(16))) guCmdList[GU_CMDLIST_SIZE];
+
+void *psp_screen = VRAM_FB0;
+
+static int current_screen = 0; /* front bufer */
+
+#if defined(LOG_TO_FILE)
+static  FILE *logFile = NULL;
+
+static void log_init(void)
+{
+	logFile = fopen("log.txt", "w");
+}
+
+static void log_deinit(void)
+{
+	if (logFile)
+		fclose(logFile);
+}
+#endif
+
+static void psp_init(void)
+{
+	lprintf("\n%s\n", "PicoDrive v" VERSION " " __DATE__ " " __TIME__);
+
+	/* video */
+	sceDisplaySetMode(0, 480, 272);
+	sceDisplaySetFrameBuf(VRAM_FB1, 512, PSP_DISPLAY_PIXEL_FORMAT_565, PSP_DISPLAY_SETBUF_NEXTFRAME);
+	current_screen = 1;
+	psp_screen = VRAM_FB0;
+
+	/* gu */
+	sceGuInit();
+
+	sceGuStart(GU_DIRECT, guCmdList);
+	sceGuDrawBuffer(GU_PSM_5650, (void *)VRAMOFFS_FB0, 512);
+	sceGuDispBuffer(480, 272, (void *)VRAMOFFS_FB1, 512); // don't care
+	sceGuDepthBuffer((void *)VRAMOFFS_DEPTH, 512);
+	sceGuClearColor(0);
+	sceGuClearDepth(0);
+	sceGuClear(GU_COLOR_BUFFER_BIT | GU_DEPTH_BUFFER_BIT);
+	sceGuOffset(2048 - (480 / 2), 2048 - (272 / 2));
+	sceGuViewport(2048, 2048, 480, 272);
+	sceGuDepthRange(0xc350, 0x2710);
+	sceGuScissor(0, 0, 480, 272);
+	sceGuEnable(GU_SCISSOR_TEST);
+
+	sceGuDepthMask(0xffff);
+	sceGuDisable(GU_DEPTH_TEST);
+
+	sceGuFrontFace(GU_CW);
+	sceGuEnable(GU_TEXTURE_2D);
+	sceGuTexFunc(GU_TFX_REPLACE, GU_TCC_RGB);
+	sceGuAmbientColor(0xffffffff);
+	sceGuColor(0xffffffff);
+	sceGuFinish();
+	sceGuSync(0, 0);
+
+	sceDisplayWaitVblankStart();
+	sceGuDisplay(GU_TRUE);
+
+
+	/* input */
+	sceCtrlSetSamplingCycle(0);
+	sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
+}
+
+static void psp_finish(void)
+{
+	lprintf("psp_finish..\n");
+	sceGuTerm();
+
+	sceKernelExitGame();
+}
+
+static void psp_video_flip(int wait_vsync, int other)
+{
+	unsigned long fb = (unsigned long)psp_screen & ~0x40000000;
+	if (other) fb ^= 0x44000;
+	if (wait_vsync) sceDisplayWaitVblankStart();
+	sceDisplaySetFrameBuf((void *)fb, 512, PSP_DISPLAY_PIXEL_FORMAT_565,
+		PSP_DISPLAY_SETBUF_IMMEDIATE);
+	current_screen ^= 1;
+	psp_screen = current_screen ? VRAM_FB0 : VRAM_FB1;
+}
+
+static int exitCallback(int arg1, int arg2, void *common) 
+{
+	engineState = PGS_Quit; 
+	return 0; 
+} 
+
+static int callbackThread(SceSize args, void *argp) 
+{ 
+	int callbackID; 
+
+	callbackID = sceKernelCreateCallback("Exit Callback", exitCallback, NULL); 
+	sceKernelRegisterExitCallback(callbackID); 
+
+	sceKernelSleepThreadCB(); 
+
+	return 0; 
+} 
+
+static int setupExitCallback() 
+{ 
+	int threadID = 0; 
+
+	threadID = sceKernelCreateThread("Callback Update Thread", callbackThread, 0x11, 0xFA0, THREAD_ATTR_USER, 0); 
+	 
+	if(threadID >= 0) 
+	{ 
+		sceKernelStartThread(threadID, 0, 0); 
+	} 
+
+	return threadID; 
+}
+
+void *psp_video_get_active_fb(void)
+{
+	return current_screen ? VRAM_FB1 : VRAM_FB0;
+}
 
 /* System level intialization */
 int plat_target_init(void)
@@ -131,6 +259,10 @@ int plat_parse_arg(int argc, char *argv[], int *x)
 /* Preliminary initialization needed at program start */
 void plat_early_init(void)
 {
+	setupExitCallback();
+#if defined(LOG_TO_FILE)
+	log_init();
+#endif
 }
 
 /* base directory for configuration and save files */
@@ -209,15 +341,7 @@ void plat_sleep_ms(int ms)
 /* sleep for some time in us */
 void plat_wait_till_us(unsigned int us_to)
 {
-	unsigned int now;
-
-	now = plat_get_ticks_us();
-
-	while ((signed int)(us_to - now) > 512)
-	{
-		usleep(1024);
-		now = plat_get_ticks_us();
-	}
+	usleep(us_to - plat_get_ticks_us());
 }
 
 /* wait until some event occurs, or timeout */
@@ -295,4 +419,18 @@ int posix_memalign(void **p, size_t align, size_t size)
 	if (p)
 		*p = memalign(align, size);
 	return (p ? *p ? 0 : ENOMEM : EINVAL);
+}
+
+/* lprintf */
+void lprintf(const char *fmt, ...)
+{
+	va_list vl;
+
+	va_start(vl, fmt);
+#if defined(LOG_TO_FILE)
+	vfprintf(logFile, fmt, vl);
+#else
+	vprintf(fmt, vl);
+#endif
+	va_end(vl);
 }
